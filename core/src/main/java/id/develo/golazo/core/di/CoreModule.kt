a9bca9f3a8d6610a762736e8bf320a5cca9c382f
teddyrahsyah212 <teddyzoeldyek@gmail.com>
2021-07-24 20:37:11 +0700

    Adding SSL Pinning (thesportdb.com)

--- core/src/main/java/id/develo/golazo/core/di/CoreModule.kt
+++ core/src/main/java/id/develo/golazo/core/di/CoreModule.kt
@@ -10,6 +10,7 @@ import id.develo.golazo.core.domain.repository.ITeamRepository
 import id.develo.golazo.core.utils.AppExecutors
 import net.sqlcipher.database.SQLiteDatabase
 import net.sqlcipher.database.SupportFactory
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.android.ext.koin.androidContext
@@ -32,10 +33,17 @@ val databaseModule = module {
 
 val networkModule = module {
     single {
+        val hostname = "www.thesportsdb.com"
+        val certificatePinner = CertificatePinner.Builder()
+            .add(hostname, "sha256/Yl6IYMBnxrwtn9DWo32vSutvY3gRn56iEfWj5/66c88=")
+            .add(hostname, "sha256/FEzVOUp4dF3gI0ZVPRJhFbSJVXR+uQmMH65xhs1glH4=")
+            .add(hostname, "sha256/Y9mvm0exBk1JoQ57f9Vm28jKo5lFm/woKcVxrYxu80o=")
+            .build()
         OkHttpClient.Builder()
             .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
             .connectTimeout(120, TimeUnit.SECONDS)
             .readTimeout(120, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner)
             .build()
     }
 
